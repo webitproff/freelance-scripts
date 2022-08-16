01482a454666d75ed0d871a0b20fa2d052cf7d0a
webitproff <webitproff@gmail.com>
2022-08-16 20:53:59 +0300

    Add files via upload

---
+++ lang/en/main.en.lang.php
@@ -0,0 +1,517 @@
+<?php
+/**
+ * English Language File (main.lang.php)
+ *
+ * @package Cotonti
+ * @copyright (c) Cotonti Team
+ * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
+ */
+
+defined('COT_CODE') or die('Wrong URL.');
+
+/**
+ * Main strings
+ */
+
+$L['Access'] = 'Access';
+$L['Action'] = 'Action';
+$L['Active'] = 'Active';
+$L['Activity'] = 'Activity';
+$L['Add'] = 'Add';
+$L['Added'] = 'Added';
+$L['Admin'] = 'Admin';
+$L['Administration'] = 'Administration';
+$L['Adminpanel'] = 'Administration panel';
+$L['Age'] = 'Age';
+$L['Ago'] = 'ago';
+$L['AIM'] = 'AIM Address';
+$L['Alias'] = 'Alias';
+$L['All'] = 'All';
+$L['Allcategories'] = 'All categories';
+$L['Allpages'] = 'All pages';
+$L['Alltimesare'] = 'All times are';
+$L['Anonymous'] = 'Anonymous';
+$L['Archives'] = 'Archives';
+$L['Ascending'] = 'Ascending';
+$L['Author'] = 'Author';
+$L['Available'] = 'Available';
+$L['Avatar'] = 'Avatar';
+$L['Avatars'] = 'Avatars';
+$L['Back'] = 'Back';
+$L['Banned'] = 'Banned';
+$L['BBcode'] = 'BBcode';
+$L['BBcodes'] = 'BBcodes';
+$L['Begin'] = 'Begin';
+$L['Birthdate'] = 'Birthdate';
+$L['Birthday'] = 'Birthday';
+$L['Blog'] = 'Blog';
+$L['Blogs'] = 'Blogs';
+$L['Body'] = 'Body';
+$L['Byfirstletter'] = 'By first letter';
+$L['bytes'] = 'bytes';
+$L['Cancel'] = 'Cancel';
+$L['Categories'] = 'Categories';
+$L['Category'] = 'Category';
+$L['Clearstatus'] = 'Clear status';
+$L['Clickhere'] = 'Click here to continue...';
+$L['Close'] = 'Close';
+$L['Code'] = 'Code';
+$L['Configuration'] = 'Configuration';
+$L['Confirm'] = 'Confirm';
+$L['Copyright'] = 'Copyright';
+$L['Core'] = 'Core';
+$L['Count'] = 'Count';
+$L['Country'] = 'Country';
+$L['Create'] = 'Create';
+$L['Crumbs'] = 'You\'re here';
+$L['Custom'] = 'Custom';
+$L['Database'] = 'Database';
+$L['Date'] = 'Date';
+$L['Days'] = 'Days';
+$L['Default'] = 'Default';
+$L['Delete'] = 'Delete';
+$L['Deleted'] = 'Deleted';
+$L['Descending'] = 'Descending';
+$L['Description'] = 'Description';
+$L['Disabled'] = 'Disabled';
+$L['Done'] = 'Done';
+$L['Download'] = 'Download';
+$L['Downloaded'] = 'Downloaded';
+$L['Duplicate'] = 'Duplicate';
+$L['Duration'] = 'Duration';
+$L['Edit'] = 'Edit';
+$L['Email'] = 'Email';
+$L['Enabled'] = 'Enabled';
+$L['Entry'] = 'Entry';
+$L['Entries'] = 'Entries';
+$L['Error'] = 'Error';
+$L['Event'] = 'Event';
+$L['Events'] = 'Events';
+$L['Expand'] = 'Expand';
+$L['Expire'] = 'Expire';
+$L['Extrafield'] = 'Extra field';
+$L['File'] = 'File';
+$L['Filedunder'] = 'Filed under';
+$L['Files'] = 'Files';
+$L['Filesize'] = 'Filesize';
+$L['Filter'] = 'Filter';
+$L['Filters'] = 'Filters';
+$L['Fold'] = 'Fold';
+$L['Folder'] = 'Folder';
+$L['Folders'] = 'Folders';
+$L['Forums'] = 'Forums';
+$L['Found'] = 'Found';
+$L['Galleries'] = 'Galleries';
+$L['Gallery'] = 'Gallery';
+$L['Gender'] = 'Gender';
+$L['Ghost'] = 'Ghost';
+$L['Group'] = 'Group';
+$L['Group0'] = 'Hidden group';
+$L['Group1'] = 'Open group';
+$L['Groups'] = 'Groups';
+$L['Groupsmembership'] = 'Groups membership';
+$L['Guest'] = 'Guest';
+$L['Guests'] = 'Guests';
+$L['Headlines'] = 'View headlines';
+$L['Help'] = 'Help';
+$L['Here'] = 'Here';
+$L['Hi'] = 'Hi';
+$L['Hidden'] = 'Hidden';
+$L['Hide'] = 'Hide';
+$L['Hits'] = 'Hits';
+$L['Home'] = 'Home';
+$L['Hook'] = 'Hook';
+$L['Hooks'] = 'Hooks';
+$L['Hours'] = 'Hours';
+$L['Icon'] = 'Icon (URL)';
+$L['ICQ'] = 'ICQ Number';
+$L['Id'] = 'ID';
+$L['Image'] = 'Image';
+$L['ImageURL'] = 'Image URL';
+$L['Inactive'] = 'Inactive';
+$L['Ip'] = 'IP';
+$L['Ipsearch'] = 'IP search';
+$L['IRC'] = 'IRC';
+$L['Item'] = 'Item';
+$L['Items'] = 'Items';
+$L['JustNow'] = 'Just now';
+$L['kb'] = 'KB';
+$L['Key'] = 'Key';
+$L['Keywords'] = 'Keywords';
+$L['Language'] = 'Language';
+$L['Languages'] = 'Languages';
+$L['Lastlogged'] = 'Last logged';
+$L['Lastpost'] = 'Last post';
+$L['Left'] = 'Left';
+$L['LessThan'] = 'Less than';
+$L['Level']	= 'Level';
+$L['Levels'] = 'Levels';
+$L['Link']	= 'Link';
+$L['Links']	= 'Links';
+$L['Locale'] = 'Locale';
+$L['Location'] = 'Location';
+$L['Lock'] = 'Lock';
+$L['Locked'] = 'Locked';
+$L['Log'] = 'Log';
+$L['Login'] = 'Login';
+$L['Logout'] = 'Logout';
+$L['Main'] = 'Main';
+$L['Maingroup'] = 'Main group';
+$L['Member'] = 'Member';
+$L['Members'] = 'Members';
+$L['Members_download'] = 'This download is available for registered users only.';
+$L['Members_only'] = 'Members only';
+$L['Menu'] = 'Menu';
+$L['Menus'] = 'Menu Slots';
+$L['Message'] = 'Message';
+$L['Messages'] = 'Messages';
+$L['Minlevel'] = 'Min.level';
+$L['Minutes'] = 'Minutes';
+$L['Moderators'] = 'Moderators';
+$L['Module'] = 'Module';
+$L['Modules'] = 'Modules';
+$L['Month'] = 'month';
+$L['Months'] = 'months';
+$L['More'] = 'More';
+$L['Move'] = 'Move';
+$L['Moved'] = 'Moved';
+$L['MSN'] = 'MSN Messenger';
+$L['Myjournal'] = 'My weblog';
+$L['Mypfs'] = 'My files';
+$L['na'] = 'Not available';
+$L['nf'] = 'Not found';
+$L['Name'] = 'Name';
+$L['Names'] = 'Names';
+$L['Navigation'] = 'Navigation';
+$L['New'] = 'New';
+$L['News'] = 'News';
+$L['Newspost'] = 'News post';
+$L['Newsposts'] = 'News posts';
+$L['Next'] = 'Next page';
+$L['No'] = 'No';
+$L['Noitemsfound'] = 'No items matching your query';
+$L['None'] = 'None';
+$L['Normal'] = 'Normal';
+$L['Notes'] = 'Notes';
+$L['No_items'] = 'No items found';
+$L['Occupation'] = 'Occupation';
+$L['Of'] = 'of';
+$L['Offline'] = 'Offline';
+$L['Online'] = 'Online';
+$L['Onpage'] = 'On page';
+$L['Ontop'] = 'Top';
+$L['Open'] = 'Open';
+$L['Option'] = 'Option';
+$L['Options'] = 'Options';
+$L['Order'] = 'Order';
+$L['Ordered'] = 'Ordered';
+$L['Originalmessage'] = 'Original message';
+$L['Other'] = 'Other';
+$L['Overview'] = 'Overview';
+$L['Owner'] = 'Owner';
+$L['Page'] = 'Page';
+$L['Pages'] = 'Pages';
+$L['Parser'] = 'Parser';
+$L['Part'] = 'Part';
+$L['Parts'] = 'Parts';
+$L['Password'] = 'Password';
+$L['Path'] = 'Path';
+$L['PFS'] = 'Personal File Space';
+$L['Photo'] = 'Photo';
+$L['Photos'] = 'Photos';
+$L['Picture'] = 'Picture';
+$L['Pictures'] = 'pictures';
+$L['Plugin'] = 'Plugin';
+$L['Plugins'] = 'Plugins';
+$L['Poll'] = 'Poll';
+$L['Polls'] = 'Polls';
+$L['Poster'] = 'Poster';
+$L['Preview'] = 'Preview';
+$L['Previous'] = 'Previous page';
+$L['Private_Messages'] = 'Private messages';
+$L['Profile'] = 'Profile';
+$L['Prune'] = 'Prune';
+$L['Public'] = 'Public';
+$L['Publish'] = 'Publish';
+$L['Putinvalidationqueue'] = 'Put in validation queue';
+$L['Quote'] = 'Quote';
+$L['Rank'] = 'Rank';
+$L['Rated'] = 'Rated';
+$L['Rating'] = 'Rating';
+$L['Ratings'] = 'Ratings';
+$L['Read'] = 'Read';
+$L['ReadMore'] = 'Read more';
+$L['Reason'] = 'Reason';
+$L['Recipient'] = 'Recipient';
+$L['Recipients'] = 'Recipients';
+$L['Refresh'] = 'Refresh';
+$L['Register'] = 'Register';
+$L['Registered'] = 'Registered';
+$L['Registration'] = 'Registration';
+$L['Remove'] = 'Remove';
+$L['Replies'] = 'Replies';
+$L['Reply'] = 'Reply';
+$L['Reset'] = 'Reset';
+$L['Restore'] = 'Restore';
+$L['Resync'] = 'Resync';
+$L['Resynced'] = 'Resynced';
+$L['Right'] = 'Right';
+$L['Rights'] = 'Rights';
+$L['RSS_Feeds'] = 'RSS Feeds';
+$L['Save'] = 'Save';
+$L['Saveasdraft'] = 'Save as Draft';
+$L['Saved'] = 'Saved';
+$L['Scheme'] = 'Color scheme';
+$L['Schemes'] = 'Color schemes';
+$L['Search'] = 'Search';
+$L['Seconds'] = 'Seconds';
+$L['Section'] = 'Section';
+$L['Sections'] = 'Sections';
+$L['Security'] = 'Security';
+$L['Sender'] = 'Sender';
+$L['Senders'] = 'Senders';
+$L['Session_expired'] = 'Your session has expired. Please try again.';
+$L['Set'] = 'Set';
+$L['Settings'] = 'Settings';
+$L['Setup'] = 'Setup';
+$L['SFS'] = 'Site File Space';
+$L['Show'] = 'Show';
+$L['Signature'] = 'Signature';
+$L['Signatures'] = 'Signatures';
+$L['Size'] = 'Size';
+$L['Smilies'] = 'Smilies';
+$L['Spoiler'] = 'Spoiler';
+$L['Standalone'] = 'Standalone';
+$L['Started'] = 'Started';
+$L['Statistics'] = 'Statistics';
+$L['StatsLogs'] = 'Statistics & logs';
+$L['Status'] = 'Status';
+$L['Structure'] = 'Structure';
+$L['Subforum'] = 'Subforum';
+$L['Subforums'] = 'Subforums';
+$L['Subject'] = 'Subject';
+$L['Submit'] = 'Submit';
+$L['Submitforapproval'] = 'Submit for approval';
+$L['Submitnew'] = 'Submit new page';
+$L['Submittedby'] = 'Submitted by';
+$L['Summary'] = 'Summary';
+$L['System'] = 'System';
+$L['Tag'] = 'Tag';
+$L['Tags'] = 'Tags';
+$L['Text'] = 'Text';
+$L['Theme'] = 'Theme';
+$L['Themes'] = 'Themes';
+$L['Thumbnail'] = 'Thumbnail';
+$L['Thumbnails'] = 'Thumbnails';
+$L['Timezone'] = 'Timezone';
+$L['Title'] = 'Title';
+$L['Today'] = 'Today';
+$L['Tool'] = 'Tool';
+$L['Tools'] = 'Tools';
+$L['Total'] = 'Total';
+$L['Topic'] = 'Topic';
+$L['TPL'] = 'TPL';
+$L['Type'] = 'Type';
+$L['Unfold'] = 'Unfold';
+$L['Unknown'] = '?';
+$L['Unlock'] = 'Unlock';
+$L['Unlocked'] = 'Unlocked';
+$L['Until'] = 'Until';
+$L['Update'] = 'Update';
+$L['Updated'] = 'Updated';
+$L['Upload'] = 'Upload';
+$L['Uploaded'] = 'Uploaded';
+$L['URL'] = 'URL';
+$L['User'] = 'User';
+$L['Userid'] = 'User ID';
+$L['Username'] = 'Username';
+$L['Users'] = 'Users';
+$L['Validate'] = 'Validate';
+$L['Value'] = 'Value';
+$L['Values'] = 'Values';
+$L['Version'] = 'Version';
+$L['View'] = 'View';
+$L['Views'] = 'Views';
+$L['Vote'] = 'Vote';
+$L['Votes'] = 'Votes';
+$L['Warning'] = 'Warning';
+$L['Website'] = 'Website';
+$L['Week'] = 'week';
+$L['Weeks'] = 'weeks';
+$L['Wipe'] = 'Wipe';
+$L['Wipeall'] = 'Wipe all';
+$L['Write'] = 'Write';
+$L['Yes'] = 'Yes';
+$L['YIM'] = 'Yahoo Messenger';
+
+/**
+ * cot_declension arrays
+ */
+$Ls['Folders'] = "folders, folder";
+$Ls['Files'] = "files, file";
+
+$Ls['Guests'] = "guests,guest";
+$Ls['Members'] = "members,member";
+$Ls['Pages'] = "pages,page";
+$Ls['Times'] = "times,time";
+
+$Ls['Milliseconds'] = "ms,ms";
+$Ls['Seconds'] = "seconds,second";
+$Ls['Minutes'] = "minutes,minute";
+$Ls['Hours'] = "hours,hour";
+$Ls['Days'] = "days,day";
+$Ls['Weeks'] = "weeks,week";
+$Ls['Months'] = "months,month";
+$Ls['Years'] = "years,year";
+
+$Ls['Bytes'] = "B";
+$Ls['Kilobytes'] = "kB";
+$Ls['Megabytes'] = "MB";
+$Ls['Gigabytes'] = "GB";
+$Ls['Terabytes'] = "TB";
+$Ls['Kibibytes'] = "KiB";
+$Ls['Mebibytes'] = "MiB";
+$Ls['Gibibytes'] = "GiB";
+$Ls['Tebibytes'] = "TiB";
+
+/**
+ * Date / Time formats
+ */
+
+$Ldt['date_full'] = 'Y-m-d';
+$Ldt['date_medium'] = 'Y-m';
+$Ldt['date_short'] = 'm-d';
+$Ldt['date_text'] = 'F d, Y';
+$Ldt['date_fulltext'] = 'l, F d, Y';
+$Ldt['time_full'] = 'H:i:s';
+$Ldt['time_medium'] = 'G:i';
+$Ldt['time_short'] = 'i:s';
+$Ldt['time_text'] = 'g:i A';
+$Ldt['time_fulltext'] = 'g:i:s A';
+$Ldt['datetime_full'] = 'Y-m-d H:i:s';
+$Ldt['datetime_medium'] = 'Y-m-d H:i';
+$Ldt['datetime_short'] = 'm-d H:i';
+$Ldt['datetime_text'] = 'F d, Y H:i';
+$Ldt['datetime_fulltext'] = 'l, F d, Y H:i';
+$Ldt['week_full'] = 'o-\WW';
+$Ldt['week_medium'] = '\WW';
+$Ldt['week_short'] = '\WW-N';
+$Ldt['week_text'] = '\WW, l';
+$Ldt['week_fulltext'] = 'o-\WW, l';
+
+/**
+ * Days of the week
+ */
+
+$L['Monday'] = 'Monday';
+$L['Tuesday'] = 'Tuesday';
+$L['Wednesday'] = 'Wednesday';
+$L['Thursday'] = 'Thursday';
+$L['Friday'] = 'Friday';
+$L['Saturday'] = 'Saturday';
+$L['Sunday'] = 'Sunday';
+$L['Monday_s'] = 'Mon';
+$L['Tuesday_s'] = 'Tue';
+$L['Wednesday_s'] = 'Wed';
+$L['Thursday_s'] = 'Thu';
+$L['Friday_s'] = 'Fri';
+$L['Saturday_s'] = 'Sat';
+$L['Sunday_s'] = 'Sun';
+
+/**
+ * Months
+ */
+
+$L['January'] = 'January';
+$L['February'] = 'February';
+$L['March'] = 'March';
+$L['April'] = 'April';
+$L['May'] = 'May';
+$L['June'] = 'June';
+$L['July'] = 'July';
+$L['August'] = 'August';
+$L['September'] = 'September';
+$L['October'] = 'October';
+$L['November'] = 'November';
+$L['December'] = 'December';
+$L['January_s'] = 'Jan';
+$L['February_s'] = 'Feb';
+$L['March_s'] = 'Mar';
+$L['April_s'] = 'Apr';
+$L['May_s'] = 'May';
+$L['June_s'] = 'Jun';
+$L['July_s'] = 'Jul';
+$L['August_s'] = 'Aug';
+$L['September_s'] = 'Sep';
+$L['October_s'] = 'Oct';
+$L['November_s'] = 'Nov';
+$L['December_s'] = 'Dec';
+
+/**
+ * Number formatting
+ */
+
+$Ln['decimal_point'] = '.';
+$Ln['thousands_separator'] = ',';
+
+/**
+ * Gender
+ */
+
+$L['Gender_F'] = 'Female';
+$L['Gender_M'] = 'Male';
+$L['Gender_U'] = 'Unknown';
+
+/**
+ * Common Entries
+ */
+
+$L['com_themefail'] = 'Warning, failed to load your prefered theme, check your profile';
+$L['com_schemefail'] = 'Warning, failed to load your prefered color scheme for the prefered theme, please check your profile and update values';
+$L['com_defthemefail'] = 'Error, default theme not found, check your theme folder';
+
+/**
+ * Header
+ */
+
+$L['hea_noprivatemessages'] = 'No private messages';
+$L['hea_valqueues'] = 'In validation queues: ';
+$L['hea_viewsite'] = 'View Website';
+$L['hea_welcome'] = 'Welcome';
+$L['hea_youareloggedas'] = 'Logged in as';
+$L['hea_youarenotlogged'] = 'You\'re not logged in';
+
+/**
+ * Footer
+ */
+
+$L['foo_poweredby'] = 'Powered by';
+$L['foo_created'] = 'Page created in';
+$L['foo_sqlqueries'] = 'SQL queries';
+$L['foo_sqltotal'] = 'SQL total time';
+$L['foo_sqlaverage'] = 'Average SQL time';
+
+/**
+ * Extra fields
+ */
+$L['field_extension']  = 'Wrong file extension';
+$L['field_notinarray'] = 'Field value is not true';
+$L['field_pregmatch'] = 'Field value is not true';
+$L['field_range'] = 'Field value is not true';
+$L['field_required'] = 'Field must be filled';
+
+/**
+ * Miscellaneous
+ */
+
+$L['pagenav_first'] = '&lt;&lt;';
+$L['pagenav_prev'] = '&lt;';
+$L['pagenav_next'] = '&gt;';
+$L['pagenav_last'] = '&gt;&gt;';
+
+$L['ajaxSenderror'] = 'The server encountered an internal error or misconfiguration and was unable to complete your request. Please contact the administrator and inform them of the time the error occurred, and anything you might have done that may have caused the error.';
+
+$L['captcha_verification_failed'] = 'Wrong bot protection code';
+
+$L['shield_title'] = 'Anti-hammering protection';
+$L['shield_protect'] = 'Shield protection activated, please retry in {$sec} seconds...<br />After this duration, you can refresh the current page to continue.<br />Last action was : {$action}';
