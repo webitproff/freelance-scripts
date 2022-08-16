99d331530061876da3b920b8acbcbf141757d4f3
webitproff <webitproff@gmail.com>
2022-08-16 21:03:25 +0300

    Add files via upload

---
+++ page/page.structure.php
@@ -0,0 +1,18 @@
+<?php
+/* ====================
+[BEGIN_COT_EXT]
+Hooks=admin.structure.first
+[END_COT_EXT]
+==================== */
+
+/**
+ * Page module
+ *
+ * @package Page
+ * @copyright (c) Cotonti Team
+ * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
+ */
+
+defined('COT_CODE') or die('Wrong URL');
+
+$extension_structure[] = 'page';
