efeb4e21833d26d99958e9399f270f848b5460fc
webitproff <webitproff@gmail.com>
2022-08-16 21:04:02 +0300

    Delete page directory

--- page/setup/page.uninstall.sql
+++
@@ -1,8 +0,0 @@
-/**
- * Completely removes page data
- */
-
-DROP TABLE IF EXISTS `cot_pages`;
-
-DELETE FROM `cot_auth` WHERE `auth_code` = 'page';
-DELETE FROM `cot_structure` WHERE `structure_area` = 'page';
--- page/setup/patch_0.9.5.sql
+++
@@ -1,2 +0,0 @@
-/* 0.9.5 separate field for last page update */
-ALTER TABLE `cot_pages` ADD COLUMN `page_updated` int(11) NOT NULL default '0';
