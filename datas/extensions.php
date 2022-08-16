d3fbac6ae2e96ee97463b1b985fc837df4dc999f
webitproff <webitproff@gmail.com>
2022-08-16 20:09:20 +0300

    Add files via upload

---
+++ datas/extensions.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * PFS extensions
+ *
+ * @package Cotonti
+ * @copyright (c) Cotonti Team
+ * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
+ */
+
+$cot_extensions = array();
+
+$cot_extensions[] = array ('rar', 'Archive', 'rar');
+$cot_extensions[] = array ('zip', 'Archive', 'zip');
+$cot_extensions[] = array ('avi', 'Video', 'mov');
+$cot_extensions[] = array ('qt', 'Video', 'mov');
+$cot_extensions[] = array ('mov', 'Video', 'mov');
+$cot_extensions[] = array ('mpeg', 'Video', 'mov');
+$cot_extensions[] = array ('mpg', 'Video', 'mov');
+$cot_extensions[] = array ('ogg', 'Video', 'mov');
+$cot_extensions[] = array ('bmp', 'Picture', 'bmp');
+$cot_extensions[] = array ('gif', 'Picture', 'gif');
+$cot_extensions[] = array ('jpeg', 'Picture', 'jpg');
+$cot_extensions[] = array ('jpg', 'Picture', 'jpg');
+$cot_extensions[] = array ('png', 'Picture', 'png');
+$cot_extensions[] = array ('mp3', 'Music', 'mp3');
+$cot_extensions[] = array ('wav', 'Music', 'wav');
+$cot_extensions[] = array ('txt', 'Text', 'txt');
+$cot_extensions[] = array ('pdf', 'Adobe document', 'pdf');
