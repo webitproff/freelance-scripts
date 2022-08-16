da88ee4fac3c3fdce77b5c340a5587af2c18f388
webitproff <webitproff@gmail.com>
2022-08-16 22:40:09 +0300

    Add files via upload

---
+++ themes/symisun-03/symisun-03.rc.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * JavaScript registry
+ *
+ * @package SymiSun
+ * @copyright (c) Cotonti Team
+ * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
+ */
+
+defined('COT_CODE') or die('Wrong URL.');
+
+cot_rc_add_file('themes/symisun-03/symisun-03.css');
+cot_rc_add_file('themes/symisun-03/js/tabcontent.min.js');
+cot_rc_add_file('themes/symisun-03/js/resizer.min.js');
+cot_rc_add_file('themes/symisun-03/js/scroller.min.js');
+cot_rc_add_file('themes/symisun-03/js/textshadow.min.js');
