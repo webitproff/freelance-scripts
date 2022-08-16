1a6e0775ea222df9334ae83420d245d9e7570bf4
webitproff <webitproff@gmail.com>
2022-08-16 23:20:09 +0300

    Add files via upload

---
+++ plugins/ckeditor/ckeditor.setup.php
@@ -0,0 +1,21 @@
+<?php
+/* ====================
+[BEGIN_COT_EXT]
+Code=ckeditor
+Name=CKEditor
+Category=editor-parser
+Description=WYSIWYG HTML editor
+Version=1.0.3-4.19.1
+Date=2022-07-24
+Author=CKSource, https://ckeditor.com
+Copyright=(c) 2003 - 2022 CKSource Holding sp. z o.o.
+Notes=Licensed under the terms of any of the following licenses: GPL, LGPL, MPL
+Auth_guests=R
+Lock_guests=12345A
+Auth_members=RW
+Lock_members=12345A
+Recommends_plugins=htmlpurifier
+[END_COT_EXT]
+==================== */
+
+defined('COT_CODE') or die('Wrong URL');
